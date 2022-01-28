8ca081e569fc93c3cedeefe2db09ebd18ac3ed9a
Andy Shaw <andy-git@gloomy-place.com>
2022-01-28 17:17:55 +0000

    Cleanup firmware update and bring into line with F4

--- src/STM32/stm32h7.h
+++ src/STM32/stm32h7.h
@@ -2,6 +2,7 @@
 #define STM32H7_H
 #include <stm32_def.h>
 #include <PinNames.h>
-// Address of main RAM bank
+// Address of main RAM bank (excluding the area used for DMA buffers)
 #define IRAM_ADDR 0x24000000
+#define IRAM_SIZE 0x70000
 #endif
