6141751f150e3e8422385e45edcd1e251960e029
Andy Shaw <andy-git@gloomy-place.com>
2022-04-24 15:47:48 +0100

    Adjust DMA threshold so that DMA is used for TMC requests

--- src/STM32/SharedSPI/HardwareSPI.cpp
+++ src/STM32/SharedSPI/HardwareSPI.cpp
@@ -34,7 +34,7 @@ Andy - 6/8/2020
 // On the H7 we need to make sure that and dma address is within a none cached memory area
 extern uint32_t _nocache_ram_start;
 extern uint32_t _nocache_ram_end;
-static constexpr uint32_t MinDMALength = 16;
+static constexpr uint32_t MinDMALength = 4;
 #define CAN_USE_DMA(ptr, len) ((ptr) == nullptr || (((const char *)(ptr) >= (const char *)&_nocache_ram_start) && ((const char *)(ptr) + (len) < (const char *)&_nocache_ram_end)))
 
 // Create SPI devices the actual configuration is set later
