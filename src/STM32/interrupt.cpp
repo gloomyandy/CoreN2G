25ea16ce58ffccb9d81b0dec3a92a768d02ea75d
Andy Shaw <andy-git@gloomy-place.com>
2022-06-07 00:22:12 +0100

    Fixed bug when detaching an interrupt pin

--- src/STM32/interrupt.cpp
+++ src/STM32/interrupt.cpp
@@ -161,6 +161,7 @@ bool stm32_interrupt_enable(GPIO_TypeDef *port, uint16_t pin, StandardCallbackFu
   }
 #endif /* STM32F1xx */
   if (gpio_irq_conf[id].callback != NULL && gpio_irq_conf[id].callback != callback) return false;
+  //if (gpio_irq_conf[id].callback != NULL) return false;
   GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
 
   HAL_GPIO_Init(port, &GPIO_InitStruct);
