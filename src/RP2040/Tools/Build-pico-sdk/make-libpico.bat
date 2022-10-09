74656d0b0b5fbef0731269f90f4e48750aa20c30
Andy Shaw <andy-git@gloomy-place.com>
2022-10-09 19:17:14 +0100

    Temp fixes for pico sdk build

--- src/RP2040/Tools/Build-pico-sdk/make-libpico.bat
+++ src/RP2040/Tools/Build-pico-sdk/make-libpico.bat
@@ -1,9 +1,10 @@
 rmdir /S /Q build
 mkdir build
 cd build
-cmake .. -DIPV6=0 -G "MSYS Makefiles"
 set OLDPATH=%PATH%
+PATH C:\Program Files (x86)\GNU Arm Embedded Toolchain\10 2021.10\bin;%PATH%
+cmake .. -DIPV6=0 -G "MSYS Makefiles"
 PATH C:\msys64\usr\bin;%PATH%
-c:\msys64\usr\bin\make -j
+make
 PATH %OLDPATH%
 cd ..
