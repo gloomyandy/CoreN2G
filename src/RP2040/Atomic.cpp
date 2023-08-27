7993c65b261c9c4652dd1458c7c24a4965fabf1f
Andy Shaw <andy-git@gloomy-place.com>
2023-08-27 13:22:54 +0100

    Improved rp2040 support

--- src/RP2040/Atomic.cpp
+++ src/RP2040/Atomic.cpp
@@ -67,4 +67,12 @@ extern "C" unsigned int __atomic_fetch_and_4(volatile void *ptr, unsigned int va
 	return ret;
 }
 
+extern "C" unsigned int __atomic_exchange_4(volatile void *ptr, unsigned int val, int memorder) noexcept
+{
+	const irqflags_t flags = IrqSave();
+	const unsigned int ret = *(volatile unsigned int*)ptr;
+	*(volatile unsigned int*)ptr = val;
+	IrqRestore(flags);
+	return ret;
+}
 // End
