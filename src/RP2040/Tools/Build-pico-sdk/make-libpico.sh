128b76eb14c15c044e077b3f0d6239654492f244
Andy Shaw <andy-git@gloomy-place.com>
2025-08-10 11:50:47 +0100

    Fix build problems with boot2

--- src/RP2040/Tools/Build-pico-sdk/make-libpico.sh
+++ src/RP2040/Tools/Build-pico-sdk/make-libpico.sh
@@ -15,41 +15,46 @@ cd build-rp2040
 CPU=rp2040 cmake .. -DIPV6=0 -DPICO_USE_FASTEST_SUPPORTED_CLOCK=1 -G "MSYS Makefiles"
 cmake --build . -j
 
-#rm -rf boot
-#mkdir boot
-#cd boot
-#mkdir -p pico 
-#touch pico/config.h
-#for type in boot2_generic_03h boot2_is25lp080 boot2_w25q080 boot2_w25x10cl; do
-#    for div in 2 4; do
-#        arm-none-eabi-gcc -march=armv6-m -mcpu=cortex-m0plus -mthumb -O3 \
-#                         -DNDEBUG -DPICO_FLASH_SPI_CLKDIV=$div \
-#                         -c "$PICO_SDK_PATH/src/rp2040/boot_stage2/$type.S" \
-#                         -I "$PICO_SDK_PATH/src/boards/include/boards/" \
-#                         -I "$PICO_SDK_PATH/src/rp2040/hardware_regs/include/" \
-#                         -I "$PICO_SDK_PATH/src/rp2_common/pico_platform/include/" \
-#                         -I "$PICO_SDK_PATH/src/rp2_common/boot_stage2/asminclude/" \
-#                         -I "$PICO_SDK_PATH/src/rp2040/pico_platform/include/" \
-#                         -I "$PICO_SDK_PATH/src/rp2040/boot_stage2/asminclude/" \
-#                         -I .
-#
-#        arm-none-eabi-gcc -march=armv6-m -mcpu=cortex-m0plus -mthumb -O3 \
-#                          -DNDEBUG -Wl,--build-id=none --specs=nosys.specs -nostartfiles \
-#                          -Wl,--script="$PICO_SDK_PATH/src/rp2040/boot_stage2/boot_stage2.ld" \
-#                          -Wl,-Map=$type.$div.elf.map $type.o -o $type.$div.elf 
-#
-#        arm-none-eabi-objdump -h $type.$div.elf >  $type.$div.dis
-#        arm-none-eabi-objdump -d $type.$div.elf >> $type.$div.dis
-#
-#        arm-none-eabi-objcopy -Obinary $type.$div.elf $type.$div.bin
-#
-#        python "$PICO_SDK_PATH/src/rp2040/boot_stage2/pad_checksum" \
-#                   -s 0xffffffff $type.$div.bin ${type}_${div}_padded_checksum.S
-#    done
-#done
-#mv *.S ../../../../boot2/rp2040/.
+rm -rf boot
+mkdir boot
+cd boot
+mkdir -p pico
+touch pico/config.h
+cp ../generated/pico_base/pico/*.h pico/.
+for type in boot2_generic_03h boot2_is25lp080 boot2_w25q080 boot2_w25x10cl; do
+    for div in 2 4; do
+        arm-none-eabi-gcc -march=armv6-m -mcpu=cortex-m0plus -mthumb -O3 \
+                         -DNDEBUG -DPICO_FLASH_SPI_CLKDIV=$div \
+                         -c "$PICO_SDK_PATH/src/rp2040/boot_stage2/$type.S" \
+                         -I "$PICO_SDK_PATH/src/common/pico_base_headers/include/" \
+                         -I "$PICO_SDK_PATH/src/rp2_common/pico_platform_compiler/include/" \
+                         -I "$PICO_SDK_PATH/src/rp2_common/pico_platform_sections/include/" \
+                         -I "$PICO_SDK_PATH/src/rp2_common/pico_platform_panic/include/" \
+                         -I "$PICO_SDK_PATH/src/rp2_common/pico_platform_common/include/" \
+                         -I "$PICO_SDK_PATH/src/boards/include/boards/" \
+                         -I "$PICO_SDK_PATH/src/rp2040/hardware_regs/include/" \
+                         -I "$PICO_SDK_PATH/src/rp2_common/pico_platform/include/" \
+                         -I "$PICO_SDK_PATH/src/rp2_common/boot_stage2/asminclude/" \
+                         -I "$PICO_SDK_PATH/src/rp2040/pico_platform/include/" \
+                         -I "$PICO_SDK_PATH/src/rp2040/boot_stage2/asminclude/" \
+                         -I .
 
-#cd ../..
+        arm-none-eabi-gcc -march=armv6-m -mcpu=cortex-m0plus -mthumb -O3 \
+                          -DNDEBUG -Wl,--build-id=none --specs=nosys.specs -nostartfiles \
+                          -Wl,--script="$PICO_SDK_PATH/src/rp2040/boot_stage2/boot_stage2.ld" \
+                          -Wl,-Map=$type.$div.elf.map $type.o -o $type.$div.elf 
+
+        arm-none-eabi-objdump -h $type.$div.elf >  $type.$div.dis
+        arm-none-eabi-objdump -d $type.$div.elf >> $type.$div.dis
+
+        arm-none-eabi-objcopy -Obinary $type.$div.elf $type.$div.bin
+
+        python3 "$PICO_SDK_PATH/src/rp2040/boot_stage2/pad_checksum" \
+                   -s 0xffffffff $type.$div.bin ${type}_${div}_padded_checksum.S
+    done
+done
+mv *.S ../../../../boot2/rp2040/.
+cd ..
 cd ..
 rm -rf build-rp2350
 mkdir build-rp2350
